080e43ec429d8a1279370572da462c2042e237d1
arbic <teo.maybebaby@gmail.com>
2025-01-25 13:50:44 +0500

    adapted

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -8,7 +8,7 @@ export function Heading({ title }: IHeading) {
 			<h1 className='text-xs sm:text-3xl text-black dark:text:white font-medium'>
 				{title}
 			</h1>
-			<div className='my-3 h-0.5 bg-brandLinear dark:bg-border w-full' />
+			<div className='sm:my-3 my-6 h-0.5 bg-brandLinear dark:bg-border w-full' />
 		</div>
 	)
 }
