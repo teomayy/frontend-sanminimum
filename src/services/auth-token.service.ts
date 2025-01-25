b283fe1545ac207ab504aebef2603bb1dd0880c6
arbic <teo.maybebaby@gmail.com>
2025-01-25 18:41:00 +0500

    responsive

--- src/services/auth-token.service.ts
+++ src/services/auth-token.service.ts
@@ -12,7 +12,7 @@ export const getAccessToken = () => {
 
 export const saveTokenStorage = (accessToken: string) => {
 	Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
-		domain: 'localhost',
+		domain: 'mses-sanminimum.uz',
 		sameSite: 'lax',
 		expires: 1
 	})
