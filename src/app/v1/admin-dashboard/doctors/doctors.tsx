f364cb57cc28107a71ee0df562cf405e4020d27b
arbic <teo.maybebaby@gmail.com>
2025-01-25 13:53:04 +0500

    adapted

--- src/app/v1/admin-dashboard/doctors/doctors.tsx
+++ src/app/v1/admin-dashboard/doctors/doctors.tsx
@@ -74,7 +74,7 @@ export default function DoctorPage() {
 	}
 
 	return (
-		<div className='overflow-auto h-screen '>
+		<div className='overflow-auto h-screen mb-10'>
 			<Heading title='Управление докторами' />
 
 			<div className='mb-4'>
