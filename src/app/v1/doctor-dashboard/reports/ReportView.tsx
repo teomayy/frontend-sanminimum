f6d749f3f4205f00b845180ee775417a7504646f
arbic <teo.maybebaby@gmail.com>
2025-03-10 13:47:39 +0500

    add-reports button renamed

--- src/app/v1/doctor-dashboard/reports/ReportView.tsx
+++ src/app/v1/doctor-dashboard/reports/ReportView.tsx
@@ -101,7 +101,7 @@ export function ReportView() {
 						className='bg-indigo-500 text-white px-4 py-2 rounded hover:bg-indigo-600'
 						onClick={() => router.push('/v1/doctor-dashboard/create-reports')}
 					>
-						Добавить отчет
+						Добавить заявку
 					</button>
 					<button
 						className='bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600'
